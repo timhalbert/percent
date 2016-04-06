f4b8daaa1b765ff522ca0552f0b215f7bb800c21
matt ferguson <matt.ferguson314@gmail.com>
2016-04-06 19:25:19 +0100

    Add shuffle function

--- js/main.js
+++ js/main.js
@@ -25,6 +25,26 @@ $(document).ready(function() {
         return Math.random()*100 < square(ix).data('pct');
     }
 
+    function shuffle(array) {
+        let counter = array.length;
+
+        // While there are elements in the array
+        while (counter > 0) {
+            // Pick a random index
+            let index = Math.floor(Math.random() * counter);
+
+            // Decrease counter by 1
+            counter--;
+
+            // And swap the last element with it
+            let temp = array[counter];
+            array[counter] = array[index];
+            array[index] = temp;
+        }
+
+        return array;
+    }
+
     function fillSquare(ix, pct, text) {
         $$in.eq(ix).html(
             '<span class="pct">' +
